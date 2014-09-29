46d85f3e0311a1f343904e21dc77a25bae1ae3b0
ludoch <ludovic.champenois@gmail.com>
2014-09-29 16:31:07 -0700

    .

--- src/main/java/com/google/appengine/demos/mandelbrot/ServletToDebug.java
+++ src/main/java/com/google/appengine/demos/mandelbrot/ServletToDebug.java
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author ludo
  */
-@WebServlet(name = "ServletToDebug", urlPatterns = {"/ServletToDebug"})
 public class ServletToDebug extends HttpServlet {
 
   /**
